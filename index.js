40bae69c0ffb85ffc53a2d426c45753251f2ecb1
chris90483 <chris9898@live.nl>
2021-06-01 17:02:11 +0200

    more stuff yay

--- index.js
+++ index.js
@@ -2,46 +2,63 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+let lastMessage = undefined;
 
-function check(channel) {
-    channel.messages.fetch({limit: 2})
-        .then(messages => {
-            if (messages.length < 2) return;
-            let curr = undefined; 
-            let prev = undefined;
-            messages.forEach(fetchedMessage => {
-                if (fetchedMessage.id === channel.lastMessageID) {
-                    curr = fetchedMessage;
-                } else {
-                    prev = fetchedMessage;
-                }
-            });
-            console.log(`curr: ${curr.content} prev: ${prev.content}`);
-           
-           currInt = parseInt(curr);
-           prevInt = parseInt(prev);
-           if (isNaN(currInt)) {
-               console.log('Couldn\'t parse message as int, deleting...');
-               curr.delete();
-           }
-           if (currInt != prevInt + 1) {
-               console.log('Wrong number, deleting...');
-               curr.delete();
-           }
-        })
-        .catch(error => {
-            console.log("    ERROR: " + error);
-        });
+function isNotNaN(number) {
+   if (isNaN(number)) {
+       console.log('❌ Message is NaN');
+       return false;
+   }
+   return true;
+}
+
+function isNextNumber(currInt, prevInt) {
+    if (currInt !== prevInt + 1) {
+       console.log('❌ Message is not previous + 1');
+       return false;
+    }
+    return true;
+}
+
+function isDifferentMember(message) {
+    if (message.member.id === lastMessage.member.id) {
+       console.log('❌ Message is not authored by a different member.');
+       return false;
+    }
+    return true;
+}
+
+function check(message) {
+    if (lastMessage === undefined || lastMessage === null) {
+        lastMessage = message;
+        return;
+    }
+   currInt = parseInt(message.content);
+   prevInt = parseInt(lastMessage.content);
+   
+   const allChecksPassed = isNotNaN(currInt) 
+                        && isNextNumber(currInt, prevInt) 
+                        && isDifferentMember(message);
+   
+   if (allChecksPassed) {
+        lastMessage = message;
+        lastMessage.react('✅');
+   } else {
+       message.delete();
+   }
 }
 
 client.once('ready', () => {
-   console.log('My body is ready'); 
+    console.log('My body is ready. Fetching the last message..');
+    lastMessage = client.channels.cache.find(channel => channel.id == process.env.CHANNEL_ID).lastMessage;
+    console.log(lastMessage.content);
 });
+    
 
 client.on('message', message => {
     if (message.channel.name === "the-million-channel") {
         console.log('Hey, it\'s a message in the million channel: ' + message.content);
-        check(message.channel);
+        check(message);
     }
 });
 
