7ae91fe898a438df3baab647654db8d40e480f56
chris90483 <chris9898@live.nl>
2021-06-01 21:53:29 +0200

    wooh het werkt denk ik

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 
+const userStats = require('./data/userStats');
+const fs = require('fs');
+const nodeCleanup = require('node-cleanup');
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 let lastMessage = undefined;
 
@@ -21,20 +25,55 @@ function isNextNumber(currInt, prevInt) {
 }
 
 function isDifferentMember(message) {
-    if (message.member.id === lastMessage.member.id) {
+    if (lastMessage && lastMessage.author.bot) {
+        return true;
+    }
+    if (lastMessage && message.member.id === lastMessage.member.id) {
        console.log('❌ Message is not authored by a different member.');
        return false;
     }
     return true;
 }
 
+function getLastInt() {
+    if (lastMessage !== undefined || lastMessage !== null) {
+        const result = parseInt(lastMessage.content);
+        if (isNotNaN(result)) {
+            return result;
+        }
+    }
+    return -1;
+}
+
+function showStats(message) {
+    const theirStats = userStats.hasOwnProperty(message.author.id) ? userStats[message.author.id] : {}
+    botMessage = `Stats for ${message.author.username}#${message.author.discriminator}: \n`;
+    for (const prop in theirStats) {
+        botMessage += `  ${prop}: ${theirStats[prop]}\n`;
+    }
+    message.channel.send(botMessage);
+}
+
+function addStats(message) {
+    if (userStats.hasOwnProperty(message.author.id)) {
+        userStats[message.author.id].hasOwnProperty('amountCounted') ? 
+            userStats[message.author.id].amountCounted += 1 : 
+            userStats[message.author.id].amountCounted = 1;
+    } else {
+        userStats[message.author.id] = {"amountCounted" : 1};
+    }
+}
+
 function check(message) {
+    if(message.author.bot) return;
+    console.log(`User ${message.author.username} sent ${message.content}`);
     if (lastMessage === undefined || lastMessage === null) {
+        console.log(`bug: lastMessage: ${lastMessage}`);
         lastMessage = message;
         return;
     }
    currInt = parseInt(message.content);
-   prevInt = parseInt(lastMessage.content);
+   prevInt = getLastInt();
    
    const allChecksPassed = isNotNaN(currInt) 
                         && isNextNumber(currInt, prevInt) 
@@ -42,6 +81,11 @@ function check(message) {
    
    if (allChecksPassed) {
         lastMessage = message;
+        if (message.content.toLowerCase().indexOf("!stats") !== -1) {
+            showStats(message);
+        }
+        
+        addStats(message);
         lastMessage.react('✅');
    } else {
        message.delete();
@@ -49,17 +93,31 @@ function check(message) {
 }
 
 client.once('ready', () => {
-    console.log('My body is ready. Fetching the last message..');
-    lastMessage = client.channels.cache.find(channel => channel.id == process.env.CHANNEL_ID).lastMessage;
-    console.log(lastMessage.content);
+    console.log('Connection successful. Fetching the last message..');
+    const millionChannel = client.channels.cache.find(channel => channel.name.toLowerCase() == "the-million-channel");
+    
+    let fetchedMessages = undefined;
+    millionChannel.messages.fetch({limit : 2})
+        .then(messages => {
+            console.log(`Found it! It's ${messages.filter(message => !message.author.bot).first().content}`);
+            lastMessage = messages.filter(message => !message.author.bot).first()
+        })
+        .catch(console.error);
+    setTimeout(() => {console.log(lastMessage.content);}, 1000);
 });
     
 
 client.on('message', message => {
     if (message.channel.name === "the-million-channel") {
-        console.log('Hey, it\'s a message in the million channel: ' + message.content);
         check(message);
     }
 });
 
 client.login(process.env.TOKEN);
+
+nodeCleanup((exitCode, signal) => {
+    fs.writeFileSync('./data/userStats.json', JSON.stringify(userStats), function(err) {
+		if (err) console.error(`${err}`);
+        console.log("Saved user stats, exiting.");
+	});
+});
